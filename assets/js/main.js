81e24ab9aa08ec2e00f1e7011456feb086bd59d6
barefootfunk <bryant@barefootfunk.com>
2019-11-26 16:12:56 -0600

    Style changes; Content additions; Style middle breakpoint; Fine-tune 11 sizing; Fallback animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,32 @@
+
 var Main = (function($) {
 
+  // Misc. Globals
   var screen_width = 0,
       $document,
-      documentElementStyle;
+      headerElementStyle,
+      displayCoordsPreface;
+
+  // For throttling
+  lastMove = 0;
+  eventThrottle = 10;
 
   // User sensor data
   var userX = 0.8; // I just like the way 0.8,0.8 looks. Default there before user interaction
   var userY = 0.8;
 
   // Debugging Options / Vars
-  var enableAxis = false;
-  var enableKeyPositioning = false;
+  var forceFallback = false;
   var displayCoords = false;
   var displayBoxes = false;
 
+  // Fallback
+  var useFallback = true; // default
 
   function _init() {
 
     $document = $(document);
-    documentElementStyle = document.documentElement.style;
+    headerElementStyle = $('.site-header')[0].style;
 
     passUserCoordsToCSS();
 
@@ -28,17 +36,26 @@ var Main = (function($) {
     // Set debugging vars based on query string
     readDebuggingVars();
 
-    // Determine appropriate sensor
-    initUserPositioning();
+    // Force fallback animation if debug variable says so
+    // or if we are on iOS.  iOS seems to crash doing all the accel detection and 
+    // variable font stuff.  Ran out of time to figure out why.  But iOS who's accel
+    // actually works is such a sliver of use cases.  Whatever, fallback anim!
+    if(forceFallback || iOS()) {
+      initFallback();
+    } else {
+      initUserPositioning(); // Note: If we choose accelerometer mode and the deviceOrientation fails to get events, fallback animation will happen
+    }
 
-    // Esc handlers
-    $(document).keyup(function(e) {
-      if (e.keyCode === 27) {
-        // Esc
-      }
-    });
+    // Create a nicer load-in experience, wait for images...
+    $('body').waitForImages(function() {
+      loaded();
+    }, false, true);
 
   } // end init()
+
+  function loaded() {
+    $('body').addClass('-loaded');
+  }
 
   // Called in quick succession as window is resized
   function _resize() {
@@ -60,9 +77,9 @@ var Main = (function($) {
     // See if any of the debugging options are present in query string vars
     if ( typeof vars.coords !== 'undefined' ) { displayCoords = vars.coords; }
     if ( typeof vars.boxes !== 'undefined' ) { displayBoxes = vars.boxes; }
+    if ( typeof vars.fallback !== 'undefined' ) { forceFallback = vars.fallback; }
 
     // Init any debugging options
-
 
     // UI for testing acceleromater
     if(displayCoords) { $('body').append('<div class="user-coords"></div>'); }
@@ -71,87 +88,136 @@ var Main = (function($) {
     if(displayBoxes) { $('body').addClass('debug-boxes'); }
   }
 
-  // Update CSS withour coords
+  // Detect iOS.  Thanks: https://stackoverflow.com/questions/9038625/detect-if-device-is-ios
+  function iOS() {
+
+    var iDevices = [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod'
+    ];
+
+    if (!!navigator.platform) {
+      while (iDevices.length) {
+        if (navigator.platform === iDevices.pop()){ return true; }
+      }
+    }
+
+    return false;
+  }
+
+  // Update CSS with our coords
   function passUserCoordsToCSS() {
-    documentElementStyle.setProperty('--user-x', userX);
-    documentElementStyle.setProperty('--user-y', userY);
+    headerElementStyle.setProperty('--user-x', userX);
+    headerElementStyle.setProperty('--user-y', userY);
+  }
+
+  // Read DeviceOrientation to user-x/y
+  function readDeviceOrientation(e) {
+
+    preventFallback();
+
+    // Throttle -- Thanks Matt!
+    var now = Date.now();
+    if (now > lastMove + eventThrottle) {
+      lastMove = now;
+
+      // Adjust position based on phone's angles
+      var gammaUnit = (e.gamma+45)/90; // 45deg rotation in either direction gives range [0,1]
+      var gammaUnitAdjusted = gammaUnit/2+0.25; // Map to [.25,.75]
+      userX = Math.min(Math.max( gammaUnitAdjusted ,0),1); // Create strict [0,1] limit, jic
+
+      var betaUnit = (e.beta)/90; // 0-90deg rotation gives range [0,1]
+      var betaUnitAdjusted = betaUnit; // No adjustment as of yet.
+      userY = Math.min(Math.max( betaUnitAdjusted ,0),1); // Create strict [0,1] limit, jic
+
+      // Update debugging UI
+      if(displayCoords) {
+        $('.user-coords').empty().append(displayCoordsPreface+'<br>Gamma: '+e.gamma.toFixed(2)+'<br>Unit: '+gammaUnit.toFixed(2)+'<br>Adjusted: '+gammaUnitAdjusted.toFixed(2)+'<br>userX: '+userX.toFixed(2)+'<br>Beta: '+e.beta.toFixed(2)+'<br>userY: '+userY.toFixed(2)+'<br>');
+      }
+
+      // Let CSS know
+      passUserCoordsToCSS();
+    }
+  }
+
+  // Read mouse data to user-x/y
+  function readMouse(e) {
+    e.preventDefault();
+
+    // Throttle (thanks Matt!)
+    var now = Date.now();
+    if (now > lastMove + eventThrottle) {
+      lastMove = now;
+
+      // Get vars
+      var mouseX = e.clientX;
+      var mouseY = e.clientY;
+      var windowWidth = window.innerWidth;
+      var windowHeight = window.innerHeight;
+
+      // Map mouse x position to continuum [0,1]
+      userX = (mouseX/windowWidth);
+      userY = (mouseY/windowHeight);
+
+      // Update debugging UI
+      if(displayCoords) {
+        $('.user-coords').empty().append(displayCoordsPreface+'<br>userX: '+userX.toFixed(2)+'<br>userY: '+userY.toFixed(2)+'<br>');
+      }
+
+      passUserCoordsToCSS();
+    }
   }
 
   // Get the position of the users sensor (mouse of phone accelerometer) and map in a unit cartesian space [0,1] for both x,y
   function initUserPositioning() {
 
-    // For throttling
-    var lastMove = 0;
-    var eventThrottle = 10;
-
     // UserX,Y will be given used to animate and will be read from accelerometers on devices that have those AND have touch screens
     // Otherwise we use mouse!
     // Note: Many laptops have accelerometers (hence the necessity of detecting touch)
     // It's not perfect but its the best I can figure...
+
+    // First, do we have DeviceOrientationEvent and touchscreen?
     if (window.DeviceOrientationEvent && Modernizr.touchevents) {
 
-      window.addEventListener("deviceorientation", function (event) {
+      // Update the debugging UI
+      if (displayCoords) {
+        displayCoordsPreface = 'Using orientation<br>'+(window.DeviceOrientationEvent ? 'Orientation Supported': 'No Orientation Event')+'<br>'+(Modernizr.touchevents ? 'Touch': 'No Touch');
+        $('.user-coords').empty().append(displayCoordsPreface);
+      }
 
-        // Throttle (thanks Matt!)
-        var now = Date.now();
-        if (now > lastMove + eventThrottle) {
-          lastMove = now;
+      // Attach readDeviceOrientation to Accel
+      window.addEventListener("deviceorientation", readDeviceOrientation);
 
-          // Adjust position based on phone's angles
-          var gammaUnit = (event.gamma+45)/90; // 45deg rotation in either direction gives range [0,1]
-          var gammaUnitAdjusted = gammaUnit/2+0.25; // Map to [.25,.75]
-          userX = Math.min(Math.max( gammaUnitAdjusted ,0),1); // Create strict [0,1] limit, jic
+      // Pretty immediately try to fallback.  DeviceOrientation, if its working, will immediatly halt the fallback.  If not, we get a dance.
+      fallbackTimer = setTimeout(initFallback, 50);
 
-          var betaUnit = (event.beta)/90; // 0-90deg rotation gives range [0,1]
-          var betaUnitAdjusted = betaUnit; // No adjustment as of yet.
-          userY = Math.min(Math.max( betaUnitAdjusted ,0),1); // Create strict [0,1] limit, jic
-
-          // Update debugging UI
-          if(displayCoords) {
-            $('.user-coords').empty().append('Gamma: '+event.gamma.toFixed(2)+'<br>Unit: '+gammaUnit.toFixed(2)+'<br>Adjusted: '+gammaUnitAdjusted.toFixed(2)+'<br>userX: '+userX.toFixed(2)+'<br>Beta: '+event.beta.toFixed(2)+'<br>userY: '+userY.toFixed(2)+'<br>');
-          }
-
-          // Let CSS know
-          passUserCoordsToCSS();
-        }
-      }, false);
-
+    // No?  Let's use the mouse!  (If by some chance we got here on a phone, the timeout animation process will have our back...)
     } else {
 
-      var displayCoordsPreface='';
       // Update the debugging UI
       if (displayCoords) {
         displayCoordsPreface = 'Using mouse<br>'+(window.DeviceOrientationEvent ? 'Orientation Supported': 'No Orientation Event')+'<br>'+(Modernizr.touchevents ? 'Touch': 'No Touch');
+        $('.user-coords').empty().append(displayCoordsPreface);
       }
 
       // Adjust position based on mouse (if present)
-      $(document).on('mousemove', function(e) {
-        e.preventDefault();
-
-        // Throttle (thanks Matt!)
-        var now = Date.now();
-        if (now > lastMove + eventThrottle) {
-          lastMove = now;
-
-          // Get vars
-          var mouseX = e.clientX;
-          var mouseY = e.clientY;
-          var windowWidth = window.innerWidth;
-          var windowHeight = window.innerHeight;
-
-          // Map mouse x position to continuum [0,1]
-          userX = (mouseX/windowWidth);
-          userY = (mouseY/windowHeight);
-
-          // Update debugging UI
-          if(displayCoords) {
-            $('.user-coords').empty().append(displayCoordsPreface+'<br>userX: '+userX.toFixed(2)+'<br>userY: '+userY.toFixed(2)+'<br>');
-          }
-
-          passUserCoordsToCSS();
-        }
-      });
+      $(document).on('mousemove', readMouse);
     }
+  }
+
+  function initFallback() {
+    if(useFallback) {
+      $('body').addClass('-fallback-anim');
+    }
+  }
+
+  function preventFallback() {
+    useFallback = false;
+    $('body').removeClass('-fallback-anim'); // Just in case we got here late
   }
 
   // Public functions
--- index.php
+++ index.php
@@ -51,6 +51,7 @@
     <link rel="shortcut icon" type="image/ico" href="assets/images/favicon.png">
   </head>
   <body>
+    <div class="load-screen" aria-hidden="true"><div class="load-text">Loading...</div></div>
     <header class="site-header">
       <div class="header-wrap">  
         <h1 class="title">Typeforce 11</h1>
@@ -64,47 +65,50 @@
         </div>
         <p class="announcement">
           Deadline<br>
-          1 — 3 — 20
+          1 — 3 — 20<br>
+          <br>
+          Opening Night<br>
+          2 — 21 — 20
         </p>
       </div>
     </header>
     <div class="content-wrap">
       <main class="site-main row">
-        <div class="half module">
+        <div class="module half-lg">
           <h2><a href="https://typeforce.com">Typeforce</a></h2>
           <p>An annual exhibit celebrating wildly talented, emerging typographic artists and designers.</p>
           <h2>Eligibility</h2>
           <p>All type and typography related entries are eligible. This is not a Typeface design exhibition, so please only include work that’s gallery friendly. Ideal submissions are a series of pieces or enough work to take up a 10'×10' wall. Submissions can be by anyone associated with the entry (studio or individual).</p>
         </div>
-        <div class="quarter module">
+        <div class="module quarter-lg two-thirds-md" id="contact">
           <h2>Contact</h2>
           <p>
             For questions email:<br>
             <a href="mailto:info@typeforce.com">info@typeforce.com</a>
           </p>
         </div>
-        <div class="quarter module">
+        <div class="module quarter-lg third-md" id="social">
           <h2 class="sr-only">Social</h2>
           <ul class="semantic-only-list">
             <li><a href="https://www.instagram.com/typeforce/">Instagram</a></li>
-            <li><a href="https://www.facebook.com/typeforce">Facebook</a></li>
-            <li><a href="#">Eventbrite</a></li>
+            <li><a href="https://www.facebook.com/events/3846945538664346/">Facebook</a></li>
+            <li><a href="https://www.eventbrite.com/e/typeforce-11-opening-night-tickets-83885853847">Eventbrite</a></li>
           </ul>
         </div>
       </main>
       <footer class="site-footer row">
-        <div class="half module">
+        <div class="module half-lg third-md">
           <h2>Sponsors</h2>
-          <p>Public Media Institute</p>
+          <p><a href="http://www.lumpen.com/" target="_blank">Public Media Institute</a></p>
         </div>
-        <div class="quarter module">
+        <div class="module quarter-lg third-md">
           <h2>Design</h2>
-          <p><a href="https://www.instagram.com/nmoufti">Nermin Moufti</a> + <a href="https://www.instagram.com/q_type">Will Miller</a></p>
+          <p><a target="_blank" href="https://www.instagram.com/nmoufti">Nermin Moufti</a> + <a target="_blank" href="https://www.instagram.com/q_type">Will Miller</a></p>
           <h2>Development</h2>
-          <p><a href="https://barefootfunk.com">Bryant Smith</a></p>
+          <p><a target="_blank" href="https://barefootfunk.com">Bryant Smith</a></p>
         </div>
-        <div class="quarter module">
-          <p><a href="https://typeforce.com">©Typeforce <?= date("Y") ?></a></p>
+        <div class="module quarter-lg third-md">
+          <p><a target="_blank" href="https://typeforce.com">©Typeforce <?= date("Y") ?></a></p>
         </div>
       </footer>
     </div>
